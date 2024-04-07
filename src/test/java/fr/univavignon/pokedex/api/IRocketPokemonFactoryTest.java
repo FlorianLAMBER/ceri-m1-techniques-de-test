e9428ade0812d29d4819029f5bc0a568c94b650b
Florian Lambert <flo2009@laposte.net>
2024-04-07 16:07:46 +0200

    Fin TP5 et TP6

---
+++ src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
@@ -0,0 +1,33 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IRocketPokemonFactoryTest {
+
+    private RocketPokemonFactory pokemonFactoryMock;
+
+    @Before
+    public void setUp() {
+        pokemonFactoryMock = new RocketPokemonFactory();
+        Pokemon expectedPokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4,56);
+        //when(pokemonFactoryMock.createPokemon(1, 45, 49, 49,0)).thenReturn(expectedPokemon);
+    }
+
+    @Test
+    public void testCreatePokemon() {
+        Pokemon expectedPokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4,56);
+
+        Pokemon actualPokemon = pokemonFactoryMock.createPokemon(0, 613, 64, 4000,4);
+
+        assertEquals(expectedPokemon.getDust(), actualPokemon.getDust());
+        assertEquals(expectedPokemon.getHp(), actualPokemon.getHp());
+        assertEquals(expectedPokemon.getIndex(), actualPokemon.getIndex());
+        assertEquals(expectedPokemon.getName(), actualPokemon.getName());
+        assertEquals(expectedPokemon.getAttack(), actualPokemon.getAttack());
+        assertEquals(expectedPokemon.getDefense(), actualPokemon.getDefense());
+        assertEquals(expectedPokemon.getStamina(), actualPokemon.getStamina());
+    }
+}
